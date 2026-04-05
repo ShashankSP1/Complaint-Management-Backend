8890d14f5cc3e427ffcc25d91fc86eaae1b9a7ed
ShashankSP1 <shashankshiva6361@gmail.com>
2026-04-05 17:56:32 +0530

    Initial backend setup with TypeScript and PostgreSQL

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import type {Application, Request, Response} from "express";
+import cors from "cors";
+
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("API running...");
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+import sequelize from "./config/sequelize.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const startServer = async () => {
+  try {
+    await sequelize.sync({ alter: true });
+    await sequelize.authenticate();
+    console.log("PostgreSQL Connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("DB connection failed:", error);
+  }
+};
+
+startServer();
